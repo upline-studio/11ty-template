8689c4075984fe1e4767c40270cd7558ee7914e4
vail <ysywix@gmail.com>
2024-10-09 17:20:12 +0400

    feat: migrate to 11ty ver. 3

--- vite.config.cjs
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('vite');
-
-module.exports = defineConfig({
-  resolve: {
-    alias: {
-      '@': require('node:path').resolve(__dirname, 'src/resources'),
-    },
-  },
-});

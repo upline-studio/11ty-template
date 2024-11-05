0191eda4217993c44dd045722b6b14378e72687f
bator <batr.fly@mail.ru>
2024-11-05 09:38:07 +0000

    feat: carousel macros

--- src/resources/js/app.ts
+++ src/resources/js/app.ts
@@ -1,4 +1,30 @@
+import { Navigation, Pagination } from 'swiper/modules';
+import type { SwiperOptions } from 'swiper/types';
+import { initSwiper } from '@/js/swiper.ts';
+
 const data = { a: 123 };
 
 /* eslint-disable no-console */
 console.log(data.a);
+
+const CAROUSEL_OPTIONS: Record<string, SwiperOptions> = {
+  default: {
+    modules: [Navigation, Pagination],
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      clickable: true,
+    },
+  },
+};
+function getCarouselOptions(name: string) {
+  return {
+    ...CAROUSEL_OPTIONS[name],
+  };
+}
+
+const carouselElements = document.querySelectorAll<HTMLElement>('.js-carousel');
+for (const carouselElement of carouselElements) {
+  initSwiper(carouselElement, getCarouselOptions(carouselElement.dataset.carouselName ?? 'default'));
+}
